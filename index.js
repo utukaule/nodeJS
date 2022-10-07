9f7bd2a129c36992dce2c8575d26dbf57246a00c
Utkatsh Kaule <utkarshkaule7@gmail.com>
2022-10-07 09:12:05 +0530

    file system
    fs basics

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const input = process.argv;
 
-if(input[2]=='add'){
-    fs.writeFileSync(input[3],input[4]);
+if (input[2] == 'add') {
+    fs.writeFileSync(input[3], input[4]);
 }
-else if(input[2] == 'remove'){
-    fs.unlinkSync(input[3]);
+else if (input[2] == 'remove') {
+    fs.unlinkSync(input[3]); 
 }
