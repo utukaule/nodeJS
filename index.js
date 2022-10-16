c95fbc507c51b730a93b7c771c219fb0023ee656
Utkatsh Kaule <utkarshkaule7@gmail.com>
2022-10-16 12:09:48 +0530

    how node js works
    How node js works complete.
    Reffer the notes

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("start") //first
+
+setTimeout(()=>{                    //4th
+    console.log("2 sec")
+
+},2000)
+
+
+setTimeout(()=>{                    //3rd
+    console.log("0 sec")
+
+},0)
+
+
+console.log("end")  //second
