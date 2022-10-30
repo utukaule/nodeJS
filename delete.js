ef62d767d1963a5ac91295cc298d395c878ebdcb
Utkatsh Kaule <utkarshkaule7@gmail.com>
2022-10-30 10:39:06 +0530

    delete

--- delete.js
+++ delete.js
@@ -7,5 +7,11 @@ const deleteData = async () => {
     let result = await data.deleteOne({name:'iPhone 14'})
     // let da = await data.find({}).toArray()
     console.log(result)
+    if((result.acknowledged == true) && (result.deletedCount > 0)){
+        console.log('data deleted')
+    }
+    else{
+        console.log('data not found')
+    }
 }
 deleteData();
